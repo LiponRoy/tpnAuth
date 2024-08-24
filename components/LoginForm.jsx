0488d96ac644eb0c3686a3a3260697f85f5d75e6
LiponRoy <liponbdbd@gmail.com>
2024-08-24 10:33:07 +0600

    update simple

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -29,7 +29,7 @@ export default function LoginForm() {
 				setLoading(false);
 				return;
 			}
-			router.push('/dashboard');
+			router.replace('dashboard');
 			setLoading(false);
 		} catch (error) {
 			console.log(error);
