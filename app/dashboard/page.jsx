7c4e2b1807c19d9c3f99ccdfb9b6a358014f7ef2
LiponRoy <liponbdbd@gmail.com>
2024-08-24 10:19:54 +0600

    updateee

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,12 +1,12 @@
-import UserInfo from "../../components/UserInfo";
-import React from 'react'
+import UserInfo from '../../components/UserInfo';
+import React from 'react';
 
 const page = () => {
-  return (
-    <div>
-      <UserInfo />
-    </div>
-  )
-}
+	return (
+		<div>
+			<UserInfo />
+		</div>
+	);
+};
 
-export default page
+export default page;
